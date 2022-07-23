6d2c0508263036c4674ca53f58812b282f5149e7
Dylan <dylanwong007@gmail.com>
2022-07-22 22:52:26 -0700

    Update Footer.tsx

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -8,20 +8,7 @@ import { StyledButton } from '../styles/Styles'
 class Footer extends Component {
 	render() {
 return ( 
-  <footer>
-          <div className="fixed-bottom">
-            <div className="col-md-12">
-              <ul className="pull-right list-unstyled">
-                <li>
-                    <a href="">Contact Us | Instagram | Facebook | Coin Market Cap</a>
-                </li>
-                <li>
-                    DeVo {new Date().getFullYear()}
-                </li>
-              </ul>
-            </div>
-          </div>
-      </footer>
+<div></div>
 	);
 };
 };
