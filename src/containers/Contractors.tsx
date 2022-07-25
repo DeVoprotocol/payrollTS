2d0c4673ec581c14538b89d5a68d221b0eb8fc05
Dylan <dylanwong007@gmail.com>
2022-07-25 14:11:35 -0700

    Update Contractors.tsx

--- src/containers/Contractors.tsx
+++ src/containers/Contractors.tsx
@@ -97,7 +97,7 @@ class Contractors extends Component<Props, State> {
       <div>
         <Chip label="Contractors Portal" color="primary" />
         <hr />
-          <h1><Chip label="Submit new bounty" variant="outlined" color="secondary" size="small" /></h1>
+          <h1><Chip label="Submit new reward" variant="outlined" color="secondary" size="small" /></h1>
           <div>
             <div>
               <input
